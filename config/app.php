c7206e46b0821c6dd6e657b27f1699029e2e2662
Developer <developer@google.net>
2020-12-29 18:29:14 +0300

    Add api key string to config

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'youtube_api_key' => env('YOUTUBE_API_KEY', 'Laravel'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
